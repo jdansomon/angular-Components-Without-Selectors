ae41b4a846d195a4db1e9bde77a1ca0dce4e9009
j.dansomon <j.dansomon@multivers.dev>
2023-11-23 16:24:38 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./features/home/home.component";
+import {AboutComponent} from "./features/about/about.component";
+import {ContactsComponent} from "./features/contacts/contacts.component";
+import {JoinUsComponent} from "./features/join-us/join-us.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component : HomeComponent
+  },
+  {
+    path: 'about',
+    component : AboutComponent
+  },
+  {
+    path: 'contacts',
+    component : ContactsComponent
+  },
+  {
+    path: 'join-us',
+    component : JoinUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { ContactsComponent } from './features/contacts/contacts.component';
+import { JoinUsComponent } from './features/join-us/join-us.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactsComponent,
+    JoinUsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
